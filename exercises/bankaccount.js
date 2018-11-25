f0aa14ca866389a7182a1c642820c0a26d305fad
apark105 <andypark105@gmail.com>
2018-11-24 20:30:36 -0800

    completed bank account

--- exercises/bankaccount.js
+++ exercises/bankaccount.js
@@ -3,14 +3,26 @@
 class Account{
 	constructor(){
 		//store the amount of money in the account
+		this.money = 0; 
 	}
-	add(  ){
+	add( amount ){
+		this.money = this.money + amount;
+		return this.money;
 		//add money to the amount stored in the account
 		//takes in an amount
 		//adds it to the existing amount
 		//returns the new amount in the account
 	}
 	remove( amount ){
+		if ( this.money < amount ) {
+			var remainingBal = this.money;
+			this.money = 0;
+			return remainingBal;
+		}
+		else {
+			this.money = this.money - amount 
+			return amount
+		}
 		//removes funds from an account
 		//check if the amount to withdraw is more than the account
 		//if more, only withdraw the amount in the account, not more
@@ -18,6 +30,7 @@ class Account{
 		//return the amount actually withdrawn
 	}
 	getAmount(){
+		return this.money
 		//returns the amount in the account
 	}
 }
